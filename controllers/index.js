6f69d1744ef325fdc4a7127d64f5a212febfbb19
vahyno <vahyno@gmail.com>
2018-04-02 09:24:42 -0700

    fixed front

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,5 @@
 module.exports = {
   players: require('./playersController'),                 //in object to allow export of multiple controllers
   api: require('./apiController'),
-  // albums: require('./albumsController'),
-  // albumsSongs: require('./albumsSongsController')
+  
 }
