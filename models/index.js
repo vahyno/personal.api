dd9d75325e99385eddcf1e375bc6fed8ac5bbdf0
vahyno <vahyno@gmail.com>
2018-04-01 20:59:54 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/personal_api');
+// mongoose.connect('mongodb://localhost/personal_api');
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/personal_api' );
 
 module.exports.Player = require('./players.js');
--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.post('/api/players', controllers.players.create);
 
 
 // server start
-app.listen(3000, function(){
-  console.log('HTTP server listening at localhost:3000');
-});
+app.listen(process.env.PORT || 3000)
+// app.listen(3000, function(){
+//   console.log('HTTP server listening at localhost:3000');
+// });
