4c26a4905feb7bef9085c94a307760ae8bac862d
vahyno <vahyno@gmail.com>
2018-04-01 17:07:44 -0700

    updated players seed file

--- seed.js
+++ seed.js
@@ -1,32 +1,44 @@
 var db = require('./models');
 
 
-var real_players = [{
-  name: 'teddy bear',
-  height: '5ft4',
+var over_30_players = [{
+  name: 'Misa Kvakin',
+  height: '6ft3',
   yearOfBirth: 1976,
-  skills: 'slow, big belly'
+  skills: 'euro step, hook shot, three point shot'
 },
 {
-  name: 'jim',
-  height: '5ft10',
-  yearOfBirth: 1976,
-  skills: 'fast, big belly'
-},{
-  name: 'jumper',
-  height: '5ft7',
+  name: 'Eric Haus',
+  height: '6ft5',
   yearOfBirth: 1974,
-  skills: 'good at free throws, big belly'
+  skills: 'hook shot, three point, defence'
 },{
-  name: 'rock',
-  height: '5ft',
+  name: 'Josh Stern',
+  height: '5ft7',
+  yearOfBirth: 1977,
+  skills: '3 point shooter, fast break'
+},{
+  name: 'Koala Bear',
+  height: '6ft3',
   yearOfBirth: 1975,
-  skills: 'slow, big belly'
+  skills: 'anywhere shoter, ball handeling, defence'
+},
+{
+  name: 'Baba Booey',
+  height: '5ft10',
+  yearOfBirth: 1958,
+  skills: 'big theeth, slow, no defence'
+},
+{
+name: 'Jim Bo',
+height: '6ft2',
+yearOfBirth: 1949,
+skills: 'three throws, defence'
 }];
 
 db.Player.remove({}, function(err, players){
   // code in here runs after all players are removed
-  db.Player.create(real_players, function(err, players){
+  db.Player.create(over_30_players, function(err, players){
     // code in here runs after all players are created
     if (err) { return console.log('ERROR', err); }
     console.log("all players:", players);
